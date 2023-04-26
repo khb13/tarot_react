cffa818cd60e0b3d1cd4bda5b23a1afcd46188f5
khb13 <kij5480@gmail.com>
2023-04-26 17:52:16 +0900

    20230426
    week 페이지 제작 중
    카드를 겹쳐지게 펴는 법을 모르겠음

--- src/page/Week.jsx
+++ src/page/Week.jsx
@@ -3,7 +3,41 @@ import styled from "styled-components";
 function Week() {
   return (
     <Container>
-      <TarotPlate></TarotPlate>
+      <TarotPlate>
+        <TarotLine>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+          <Between></Between>
+          <TarotCard></TarotCard>
+        </TarotLine>
+        <TarotLine>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+          <TarotCard></TarotCard>
+        </TarotLine>
+      </TarotPlate>
     </Container>
   );
 }
@@ -22,5 +56,53 @@ const TarotPlate = styled.div`
   background-color: #fef5d4;
 
   border-radius: 2%;
+`;
+
+const TarotLine = styled.div`
+  width: 24vh;
+  height: 60vh;
+
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 0px auto;
+
+  flex-direction: row;
+
+  background-color: #ddd;
+  float: left;
+
+  position: relative;
+`;
+
+const TarotCard = styled.div`
+  text-align: center;
+
+  width: 12vh;
+  height: 8vh;
+
+  margin: 20px auto 0;
+
+  border-radius: 5%;
+  border: 3px solid #fff;
+
+  background-color: #f2ab39;
+  box-shadow: 3px 3px #7f6cdc;
+
+  position: absolute;
+
+  user-select: none;
+
+  &+&: {
+  }
+`;
+
+const Between = styled.div`
+  width: 12vh;
+  height: 2vh;
+
+  position: absolute;
+
+  background-color: #000;
 `;
 export default Week;
